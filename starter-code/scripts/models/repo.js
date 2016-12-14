fa8472a0fdbe9d0f22235cbcbdba98a3c6493d25
Ashley Ross <ashley.e.ross@gmail.com>
2016-12-13 16:52:34 -0800

    request repos function working properly

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -6,17 +6,17 @@
 // TODONE: create a githubToken.js file that we can use to generate our headers
          // properly.
     $.ajax({
-      url: '/github/aer03280/repositories/',
+      url: 'https://api.github.com/users/codefellows/repos',
       type: 'GET',
-      headers:{'Authorization': 'token ' + token},
-
+      headers: {'Authorization': 'token ' + token},
+      success:
+      function(data) {
+        repos.allRepos = data;
+        console.log(data);
+      }
     });
-
-    function(data) {
-      repos.allRepos = data;
-    }
-
   };
+
 
 
 
