224d1114682979ae397700245fee73efc918d28e
mikemiksch <mikemiksch@gmail.com>
2016-12-14 09:37:06 -0800

    Everything's worker

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -2,11 +2,22 @@
   var repos = {};
 
   repos.allRepos = [];
-// TODO: create a githubToken.js file that we can use to generate our headers
+// TODONE: create a githubToken.js file that we can use to generate our headers
          // properly.
   repos.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com/users/mikemiksch/repos' +
+        '?per_page=5' +
+        '&sort=updated',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + token},
+      success: function(data) {
+        repos.allRepos = data;
+        callback();
+      }
+    });
   };
 
   repos.withTheAttribute = function(myAttr) {
