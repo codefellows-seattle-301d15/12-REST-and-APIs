4d7bfca4774d7b4b79ad245ec17bfab809ee5d84
devonhackley <devonhackley@gmail.com>
2016-12-13 17:00:10 -0800

    finished the todos

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -2,18 +2,29 @@
   var repos = {};
 
   repos.allRepos = [];
-// TODO: create a githubToken.js file that we can use to generate our headers
-         // properly.
+  // TODONE: create a githubToken.js file that we can use to generate our headers
+  // properly.
   repos.requestRepos = function(callback) {
-    /* TODO: How would you like to fetch your repos? Someone say AJAX?!
-      Do not forget to call the callback! */
+    /* TODONE: How would you like to fetch your repos? Someone say AJAX?!
+    Do not forget to call the callback! */
+    $.ajax({
+      url:'https://api.github.com/users/devonhackley/repos'+ '?per_page=5' +'&sort=updated',
+
+      type:'GET',
+      headers:{'Authorization':'token ' + githubToken},
+      success:function(data) {
+        console.log(data);
+        repos.allRepos = data;
+        callback();
+      }
+    });
   };
 
   repos.withTheAttribute = function(myAttr) {
     /* NOTE: This Model method filters the full repos collection based
-        on a particular attribute. For example, you could use this
-        to filter all repos that have a forks_count, stargazers_count,
-        or watchers_count. */
+    on a particular attribute. For example, you could use this
+    to filter all repos that have a forks_count, stargazers_count,
+    or watchers_count. */
     return repos.allRepos.filter(function(aRepo) {
       return aRepo[myAttr];
     });
